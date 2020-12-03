b1c943844c6d6de16b0a51a819e0dc30825b4f08
Alexandra <AlexandraSchetchikova@yandex.ru>
2020-12-03 13:17:00 +0300

    Install Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DayScheduleComponent } from './components/day-schedule/day-schedule.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
 	declarations: [
@@ -15,6 +16,7 @@ import { HttpClientModule } from '@angular/common/http';
 		BrowserModule,
 		AppRoutingModule,
 		HttpClientModule,
+		BrowserAnimationsModule,
 	],
 	providers: [],
 	bootstrap: [AppComponent]
